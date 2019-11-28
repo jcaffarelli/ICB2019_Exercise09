03e3d76f0e3f41d3cf9c62eb69ad23a3e429283a
John Caffarelli <jcaffare@nd.edu>
2019-11-28 11:06:31 -0500

    Question 1

--- Ex09.R
+++ Ex09.R
@@ -2,7 +2,18 @@
 
 library(ggplot2)
 
-#Question 2: Plotting data.txt data
+## Question 1: Plotting my own data
+#setting working directory to lab data
+setwd("C:/Users/jcaff/Documents/Jones Lab/hsSurvey")
+#bring in lake info
+linfo=read.csv("biocom_Lakes.csv", header = T, stringsAsFactors = F) #lake characteristic
+head(linfo)
+#plot to relate lake area to lake perimeter
+ggplot(data=linfo,mapping = aes(x=perimeter,y=area))+
+  geom_point()+xlab("Lake Perimeter")+ylab("Lake Area")+stat_smooth(method="lm",color="red")+
+  theme_classic()
+
+## Question 2: Plotting data.txt data
 #loading data
 data<-read.csv("data.txt", header = TRUE)
 
@@ -12,7 +23,7 @@ ggplot(data=data, mapping = aes(x=region, y=observations))+
 
 #scatterplot of all observations
 ggplot(data=data, mapping=aes(x=region, y=observations))+
-  geom_point(aes(color=region))+xlab("MPG-City")+ylab("MPG-Highway")+stat_smooth(method="lm", color="black")+
+  geom_point(aes(color=region))+xlab("region")+ylab("observations")+stat_smooth(method="lm", color="black")+
   theme_classic()
 #The bar plot shows the means for each region, however it is difficult to determine how they are different given that+
 #the means are so similar. The scatterplot shows the distribution of the observations and how the ranges for each region vary
