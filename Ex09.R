a6f9c8dea88cf448514184c9874fdd39a792bba4
John Caffarelli <jcaffare@nd.edu>
2019-11-28 10:01:52 -0500

    Barplot for question 1

---
+++ Ex09.R
@@ -0,0 +1,12 @@
+### Exercise 09 ###
+
+library(ggplot2)
+
+#Question 2: Plotting data.txt data
+#loading data
+data<-read.csv("data.txt", header = TRUE)
+
+#barplot of the mean of the populations
+ggplot(data=data, mapping = aes(x=region, y=observations))+
+  stat_summary(geom = "bar",fun.y = "mean")+theme_classic()
+
